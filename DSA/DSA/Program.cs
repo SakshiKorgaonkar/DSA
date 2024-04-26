ebe9af455151fe4b002088c223a2b3f8dc1a6770
SakshiKorgaonkar <korgaonkarsakshi23@gmail.com>
2024-04-26 13:42:08 +0530

    [sakshi]Add: array dara structure

--- DSA/DSA/Program.cs
+++ DSA/DSA/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Immutable;
+using System.Globalization;
+using System.Linq;
+
+namespace DataStructure
+{
+    internal class Array
+    {
+        static void Main(string[] args)
+        {
+            int[] squares = new int[] { 1, 4, 9, 16, 25 };
+
+            Console.WriteLine("Original array:");
+            foreach (int i in squares)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Minimun element "+squares.Min());
+            Console.WriteLine("Maximum element "+squares.Max());
+            Console.WriteLine("Length "+ squares.Length);
+            Console.WriteLine("Rank "+squares.Rank);
+
+            squares = squares.Reverse().ToArray();
+
+            Console.WriteLine("Reversed array:");
+            foreach (int j in squares)
+            {
+                Console.WriteLine(j);
+            }
+
+            int[] numbers1 = { 4, 2, 7, 1, 9 };
+            int numberToFind = 7;
+            int foundNumber = numbers1.FirstOrDefault(n => n == numberToFind);
+
+            if (foundNumber != 0)
+            {
+                Console.WriteLine($"Found number: {foundNumber}");
+            }
+            else
+            {
+                Console.WriteLine("Number not found");
+            }
+        }
+    }
+}
--- DSA/DSA/obj/Debug/net6.0/DSA.AssemblyInfo.cs
+++ DSA/DSA/obj/Debug/net6.0/DSA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DSA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8522b074e4cdd7f5afeb977bf8dc3abeb9b43cf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("DSA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DSA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
